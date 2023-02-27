4d8b08e2358461a9580685b78a633d0a11c8de1f
Miguel Orjuela <39718400+orjuela45@users.noreply.github.com>
2023-02-27 15:00:08 -0500

    footer classname

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,11 +2,11 @@ import { SocialNetworks } from "../headerTop/SocialNetworks"
 
 export const Footer = () => {
   return (
-    <footer class="bg-dark text-center text-white">
-      <div class="container p-4 pb-0">
+    <footer className="bg-dark text-center text-white">
+      <div className="container p-4 pb-0">
         <SocialNetworks />
       </div>
-      <div class="text-center p-3" >
+      <div className="text-center p-3" >
         MIGUEL ANGEL ORJUELA RIAÑO
       </div>
     </footer>
