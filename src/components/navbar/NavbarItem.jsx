6734dcc7e632797c2d0d9de7ec819ab89f5cb2d9
Miguel Orjuela <39718400+orjuela45@users.noreply.github.com>
2023-03-01 16:51:09 -0500

    fix static cv

--- src/components/navbar/NavbarItem.jsx
+++ src/components/navbar/NavbarItem.jsx
@@ -7,7 +7,7 @@ export const NavbarItem = ({label, redirect, animation, external}) => {
   return (
     <>
       <motion.li className="nav-item col-auto" {...animation} style={{cursor: "pointer"}}>
-        <Link className="nav-link active text-center" to={redirect} smooth={true} offset={-70} duration={1000} data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show" onClick={() => external && window.open("https://drive.google.com/file/d/1pBFGLzplcSpznlsHsYJwGpkDTJmrswfV/view?usp=share_link", "_blank")}>
+        <Link className="nav-link active text-center" to={redirect} smooth={true} offset={-70} duration={1000} data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show" onClick={() => external && window.open(redirect, "_blank")}>
           <LanguageContent contentID={label} />
         </Link>
       </motion.li>
